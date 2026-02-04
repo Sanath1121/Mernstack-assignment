580e96b2bfabe133eef3cb9ce8f9ea7b42ca768a
Vemula Sanath Chandra <sanathchandra2005@gmail.com>
2026-02-04 20:07:39 +0530

    Week-4,Day 8 updated

---
+++ Week-4/Day_8/APIs/authorAPI.js
@@ -0,0 +1,10 @@
+import exp from "express";
+
+export const authorRoute=exp.Router();
+
+//Register author
+//Authenticate author
+//Create article
+//Read articles of author
+//Edit article
+//Delete(soft delete) article
---
+++ Week-4/Day_8/server.js
@@ -0,0 +1,40 @@
+import exp from "express";
+import {config} from "dotenv"
+import {connect} from "mongoose";
+import { userRoute } from "./APIs/userAPI.js";
+import { authorRoute } from "./APIs/authorAPI.js";
+import { adminRoute } from "./APIs/adminAPI.js";
+config() //process.env
+
+const app=exp();
+//body parser middleware
+app.use(exp.json())
+
+//connect apis
+app.use("/user-api",userRoute);
+app.use("/admin-api",adminRoute);
+app.use("/author-api",authorRoute);
+
+
+//connect to database
+const connectDb=async()=>{
+    try{
+        await connect(process.env.DB_URL);
+        console.log("Database connected");
+        app.listen(process.env.PORT,()=>console.log("Server started"));
+    }
+    catch(err){
+        console.log("Err in DB connection",err);
+    }
+
+}
+
+connectDb();
+
+//error handling middleware
+app.use((err,req,res,next)=>{
+    console.log("Error:",err);
+    res.json({message:"Error",error:err.message});
+    next();
+})
+
