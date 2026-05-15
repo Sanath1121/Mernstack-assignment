0c30e7bacea3302aa56038076fbb43d034eab2f9
Vemula Sanath Chandra <sanathchandra2005@gmail.com>
2026-05-15 10:50:17 +0530

    fix auth issue

--- Week-8/UserManagementApp/frontend/src/components/User.jsx
+++ Week-8/UserManagementApp/frontend/src/components/User.jsx
@@ -12,7 +12,7 @@ function User() {
   useEffect(() => {
     async function getUser() {
       try {
-        const res = await fetch(buildApiUrl(`/user-api/users/${id}`), {
+        const res = await fetch(buildApiUrl(`/user-api/${id}`), {
           method: 'GET'
         })
         const data = await res.json()
